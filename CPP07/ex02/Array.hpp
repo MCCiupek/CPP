4851f46f2c5a7f2e1bce974dc26981bfc45e5ece
MCCiupek <marieclemence.c@gmail.com>
2021-09-21 17:57:22 +0200

    CPP07

---
+++ CPP07/ex02/Array.hpp
@@ -0,0 +1,88 @@
+#ifndef __ARRAY_H__
+# define __ARRAY_H__
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+// ************************************************************************** //
+//                                Array Class                                 //
+// ************************************************************************** //
+
+template<typename T>
+class Array {
+
+public:
+
+	Array( void ) : _array(new T[0]), _size(0) {};
+	Array( unsigned int n ) : _array(new T[n]), _size(n) {};
+	Array( Array const & Copy ) { this->_array = Copy->_array; };
+	virtual ~Array( void ) { delete [] this->_array; };
+
+	Array & 		operator=( Array & Copy );
+	T				operator[]( unsigned int i );
+
+	unsigned int	size( void );
+
+	void			setArray( unsigned int i, T elem );
+
+	class 			OutOfLimits : public std::exception {};
+
+private:
+
+	T *				_array;
+	unsigned int	_size;
+
+};
+
+template<typename T>
+std::ostream&	operator<<(std::ostream& stream, Array<T> const& Array);
+
+/* ACCESSORS */
+
+template<typename T>
+void			Array<T>::setArray( unsigned int i, T elem ) {
+
+	this->_array[i] = elem;
+}
+
+/* OPERATORS */
+
+template<typename T>
+Array<T> & 	Array<T>::operator=( Array<T> & Copy ) {
+
+	this->_size = Copy.size();
+	this->_array = new T[Copy.size()];
+
+	for (unsigned int i = 0; i < this->size(); i++ )
+		this->_array[i] = Copy[i];
+	return *this;
+}
+
+template<typename T>
+T			Array<T>::operator[]( unsigned int i ) {
+
+	if (i >= this->size())
+		throw Array<T>::OutOfLimits();
+	return this->_array[i];
+}
+
+/* METHODS */
+
+template<typename T>
+unsigned int	Array<T>::size( void ) {
+
+	return this->_size;
+}
+
+template<typename T>
+std::ostream&	operator<<(std::ostream& stream, Array<T> & Array)
+{
+	if (!Array.size())
+		stream << "(null)" << std::endl;
+	for (unsigned int i = 0; i < Array.size(); i++)
+		stream << "[" << i << "]: " << Array[i] << std::endl;
+	return (stream);
+}
+
+#endif /* __ARRAY_H__ */
---
+++ CPP07/ex02/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "Array.hpp"
+#include "Human.hpp"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+
+int testIntegersArray( void ) {
+
+	Array<int> intArr;
+	Array<int> intnArr(5);
+	Array<int> intnArr2;
+	
+	for (unsigned int i = 0; i < intnArr.size(); i++)
+		intnArr.setArray(i, i);
+	intnArr2 = intnArr;
+	intnArr.setArray(0, -1);
+
+	cout << intArr << endl;
+	cout << intnArr << endl;
+	cout << intnArr2 << endl;
+
+	try {
+		cout << intnArr2[12] << endl;
+	}
+	catch (Array<int>::OutOfLimits &e) {
+		cout << "Error: index out of array limit" << endl;
+	}
+
+	return 0;
+}
+
+int testHumansArray( void ) {
+
+	Array<Human>	Band;
+	Array<Human>	Beatles(4);
+	
+	Beatles.setArray(0, Human("stuart"));
+	Beatles.setArray(1, Human("john"));
+	Beatles.setArray(2, Human("ringo"));
+	Beatles.setArray(3, Human("george"));
+	Band = Beatles;
+	Beatles.setArray(0, Human("paul"));
+
+	for (unsigned int i = 0; i < Band.size(); i++)
+		cout << Band[i].getName() << endl;
+	cout << endl;
+
+	for (unsigned int i = 0; i < Band.size(); i++)
+		cout << Beatles[i].getName() << endl;
+	cout << endl;
+
+	try {
+		cout << Beatles[5].getName() << endl;
+	}
+	catch (Array<Human>::OutOfLimits &e) {
+		cout << "Error: index out of array limit" << endl;
+	}
+
+	return 0;
+}
+
+int main( void ) {
+
+	cout << "------ Test 1: Integers ------" << endl;
+	testIntegersArray();
+	cout << endl;
+
+	cout << "------ Test 2: Humans ------" << endl;
+	testHumansArray();
+	cout << endl;
+}
